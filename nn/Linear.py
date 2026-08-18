10d8c943b7a0881ec264d1edfe5d6097440078f7
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-08-18 22:45:43 +0530

    Fixed bug in linear.py

--- nn/Linear.py
+++ nn/Linear.py
@@ -6,13 +6,9 @@ from nn.Module import Module
 class Linear(Module):
     def __init__(self, in_features, out_features):
         super().__init__()
-        bound = 1/(math.sqrt(in_features))
-        
-        weight_data = np.random.uniform(-bound, bound, (in_features, out_features))
-        self.weight = Tensor(weight_data, requires_grad=True)
-
-        bias_data = np.random.uniform(-bound, bound, (1, out_features))
-        self.bias = Tensor(bias_data, requires_grad=True)
+        random_weights = np.random.randn(in_features, out_features) * 0.01
+        self.weight = Tensor(random_weights, requires_grad=True)
+        self.bias = Tensor(np.zeros((1, out_features)), requires_grad=True)
         
     def forward(self, X):
         return (X @ self.weight) + self.bias
