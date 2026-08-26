5bec075d314252c9f0e2d7140d4e5435cf947545
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-08-26 15:10:26 +0530

    Modify  Module.py. Moves all parameters to the specified device

--- nn/Module.py
+++ nn/Module.py
@@ -1,9 +1,12 @@
 from tensor import Tensor
 
 class Module:
+    def __init__(self):
+        self.device = "cpu"
+
     def zero_grad(self):
         for p in self.parameters():
-            p.grad = 0.0
+            p.grad = None
             
     def parameters(self):
         params = []
@@ -14,6 +17,15 @@ class Module:
                 params.extend(value.parameters())
         
         return params
+    
+    def to(self, device):
+        self.device = device
+        for name, value in self.__dict__.items():
+            if isInstance(value, Tensor) and value.requires_grad:
+                setattr(self, name, value.to(device))
+            elif isInstance(value, Module):
+                value.to(device)
+        return self
     
     def forward(self, *args, **kwargs):
         raise NotImplementedError
