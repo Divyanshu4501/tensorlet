fdb0e3f1aba31a01a857a595024bf187f5551ac4
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-08-26 22:18:30 +0530

    Modify Linear.py for specific device

--- nn/Linear.py
+++ nn/Linear.py
@@ -7,8 +7,8 @@ class Linear(Module):
     def __init__(self, in_features, out_features):
         super().__init__()
         random_weights = np.random.randn(in_features, out_features) * 0.01
-        self.weight = Tensor(random_weights, requires_grad=True)
-        self.bias = Tensor(np.zeros((1, out_features)), requires_grad=True)
+        self.weight = Tensor(random_weights, requires_grad=True, device=self.device)
+        self.bias = Tensor(np.zeros((1, out_features)), requires_grad=True, device=self.device)
         
     def forward(self, X):
         return (X @ self.weight) + self.bias
