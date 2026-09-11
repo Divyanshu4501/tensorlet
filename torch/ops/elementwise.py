ed0c4dd657aa4a2e9a0e62e8c59abac0213431b3
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-09-12 00:04:49 +0530

    Add unbroadcast utility for broadcasted gradients

--- torch/ops/elementwise.py
+++ torch/ops/elementwise.py
@@ -1,4 +1,18 @@
+import numpy as np
+import cupy as cp
 from torch.autograd.function import Function
+
+def unbroadcast(grad, target_shape):
+    ndims_added = grad.ndim - len(target_shape)
+    xp = cp if isinstance(grad, cp.ndarray) else np
+    if ndims_added > 0:
+        grad = xp.sum(grad, axis = tuple(range(ndims_added)))
+    
+    for i, dim_size in enumerate(target_shape):
+        if dim_size == 1 and grad.shape[i] > 1:
+            grad = xp.sum(grad, axis = i, keepdims=True)
+            
+    return grad
 
 class Add(Function):
     def forward(self, a, b):
