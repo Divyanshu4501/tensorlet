f2c10599d8b9cc05f84ac9e1e3f51c273a8faeb3
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-09-12 13:45:22 +0530

    implement Tensor.backward (topological sort + gradient accumulation)

--- torch/tensor/tensor.py
+++ torch/tensor/tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from torch.backend import cp, HAS_CUPY
 from core.tensor_impl import TensorImpl
 from torch.ops.elementwise import Add
 
@@ -18,6 +19,38 @@ class Tensor:
     def device(self):
         return self._impl.device
     
+    def backward(self, grad = None):
+        if not self.requires_grad:
+            raise RuntimeError("element 0 of tensors does not require grad and does not have a grad_fn")
+        if grad is None:
+            grad = np.ones_like(self.data) if self.device == 'cpu' else cp.ones_like(self.data)
+        else:
+            self.grad = np.array(grad, dtype=np.float64) if self.device == 'cpu' else cp.array(grad, dtype=cp.float64)
+            
+        self.grad = grad
+        
+        topo_order = []
+        visited = set()
+        
+        def build_topo(tensor):
+            if tensor not in visited:
+                visited.add(tensor)
+                if tensor._ctx is not None: 
+                    for parent in tensor._ctx.parents:
+                       build_topo(parent) 
+                topo_order.append(tensor)
+        
+        build_topo(self)
+        for t in reversed(topo_order):
+            if t._ctx is not None:
+                 grads = t._ctx.backward(t.grad)
+                 for parent, grad_contribution in zip(t._ctx.parents, grads):
+                    if parent.grad is None:
+                         parent.grad = grad_contribution
+                    else:
+                        parent.grad = parent.grad + grad_contribution
+    
+    
     def __repr__(self):
         return f"Tensor: {self.data}, requires_grad: {self.requires_grad}"
     
@@ -30,4 +63,3 @@ class Tensor:
         result = Tensor(result_data, requires_grad=requires_grad, device=self.device)
         result._ctx = op
         return result
-        
