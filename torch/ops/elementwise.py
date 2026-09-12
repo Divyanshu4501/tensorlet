df790e7dbed8c1de08bb757fd481039f4310d25c
Divyanshu Sharma <divyanshusharma190605@gmail.com>
2026-09-13 01:27:15 +0530

    Add: Sub, Mul, Matmul, Neg, Truediv class

--- torch/ops/elementwise.py
+++ torch/ops/elementwise.py
@@ -24,5 +24,56 @@ class Add(Function):
         grad_b = unbroadcast(grad_output, b_shape)
         return grad_a, grad_b
     
+class Sub(Function):
+    def forward(self, a, b):
+        self.save_for_backward(a.shape, b.shape)
+        return a - b
     
+    def backward(self, grad_output):
+        a_shape, b_shape = self.saved_tensors
+        grad_a = unbroadcast(grad_output, a_shape)
+        grad_b = unbroadcast(-grad_output, b_shape)
+        return grad_a, grad_b
+
+class Mul(Function):
+    def forward(self, a, b):
+        self.save_for_backward(a.shape, b.shape)
+        return a*b
+    
+    def backward(self, grad_output):
+        shape_a, shape_b = self.saved_tensors
+        a = self.parents[0].data
+        b = self.parents[1].data
         
+        grad_a = unbroadcast(grad_output*b, shape_a)
+        grad_b = unbroadcast(grad_output*a, shape_b)  
+        return grad_a, grad_b
+    
+class Matmul(Function):
+    def forward(self, a, b):
+        self.save_for_backward(a, b)
+        return a@b
+            
+    def backward(self, grad_output):
+        a, b = self.saved_tensors
+        grad_a =  grad_output @ b.T
+        grad_b =  a.T  @ grad_output
+        return grad_a, grad_b
+    
+class Neg(Function):
+    def forward(self, a):
+        return -a
+    
+    def backward(self, grad_output):
+        return -grad_output
+    
+class Truediv(Function):
+    def forward(self, a, b):
+        self.save_for_backward(a, b)
+        return a/b
+    
+    def backward(self, grad_output):
+        a, b = self.saved_tensors
+        grad_a = unbroadcast(grad_output/b, a.shape)
+        grad_b = unbroadcast(-grad_output*(-a/(b**2)), b.shape)
+        return grad_a, grad_b
